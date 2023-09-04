a1e86f2378aeef050019cdf466221b197559b83a
Lance Salera <lancevincent.salera@cit.edu>
2023-09-04 15:00:08 +0800

    Final

--- main.py
+++ main.py
@@ -51,6 +51,62 @@ match problem:
 
     case 5:
         def decToBinary(dec):
+            if dec == 0:
+                return 0
+
+            binary = 0
+            temp = 1
+            while dec != 0:
+                rem = dec % 2
+                binary += rem * temp
+                temp *= 10
+                dec //= 2
+            return binary
+
+
+        def binaryToN(bin, type):
+            dec = 0
+
+            if type == 10:
+                digits = int(binary)
+                pos = 0
+                while digits > 0:
+                    rem = digits % 10
+                    dec += rem * (2 ** pos)
+                    digits //= 10
+                    pos += 1
+
+                return dec
+
+        def decToOctal(dec):
+            if dec == 0:
+                return 0
+
+            octal = 0
+            temp = 1
+            while dec != 0:
+                rem = dec % 8
+                octal += rem * temp
+                temp *= 10
+                dec //= 8
+            return octal
+
+        def defToHex(dec):
+            hexCharacters = "0123456789ABCDEF"
+            hexRes = ""
+
+            if dec == 0:
+                return "0"
+
+            while dec > 0:
+                rem = dec % 16
+                hexRes = hexCharacters[rem] + hexRes
+                dec //= 16
+
+            return hexRes
+
+        def main():
+            print("Binary to Decimal: ")
 
 
 
